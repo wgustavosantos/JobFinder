81bce96efbff9648727b3f4fdee92ef3e949e1bc
Wenderson Santos <guto15santos@gmail.com>
2022-05-29 17:11:31 -0300

    config bd add

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const db = require("./db/connection");
+
+db.authenticate()
+  .then(() => {
+    console.log("Conectou ao banco com sucesso.");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro ao conectar no banco de dados.");
+  });
 
 app.listen(PORT, () => {
   console.log(`O Express está rodando na porta ${PORT}`);
