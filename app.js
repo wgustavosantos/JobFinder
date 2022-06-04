a27be0a9ffad648ce5593d9ce85b50876fd767e2
Wenderson Santos <guto15santos@gmail.com>
2022-06-04 00:48:27 -0300

    configurando o handlebars

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const app = express();
 const PORT = 3000;
 const db = require("./db/connection");
 const bodyParser = require("body-parser");
+const exphb = require("express-handlebars");
+const path = require("path");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use("/jobs", require("./routes/jobs"));
 
-db.authenticate()
+// hadler bars
+app.set('views', path.join(__dirname, 'views')); //criar pasta views manualmente
+app.engine('handlerbars', exphb({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+db.authenticate() // conexao db
   .then(() => {
     console.log("Conectou ao banco com sucesso.");
   })
